9d1d125bc41706cdf378ba75d6b9e6b8db5178e3
ANIRBAN SANTRA <129375292+anirbansantra748@users.noreply.github.com>
2023-10-28 12:32:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quiznime.com
+
+//SECTION - FILES
+1. app.js
+2. Views - pages -> index.ejs
+
+//TODO - what to do
+<--souma-->
+date - 28.10.23
+1. make index rout a suitable coppyright free background {using ai or find anywhere}
+2. make a suitable nav bar and there have {home , quiz } in right of the nav bar make a logo of user and home also have the logo and quiz also
+date - 29.10.23
+3. make meny divs in the index rout like you visit this website to see -{https://www.beano.com/categories/quizzes}
+
---
+++ app.js
@@ -0,0 +1,38 @@
+//SECTION - requirment part
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+//SECTION - variable parts
+const app = express();
+const port = 3000;
+const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+
+//SECTION - set up all
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+async function main() {
+  await mongoose.connect(mongo_url);
+}
+
+main()
+  .then(() => {
+    console.log("Mongoose started");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//SECTION - route handling
+app.get("/", (req, res) => {
+  res.send("bhopendra jogi");
+});
+
+app.get("/quizanime", (req, res) => {
+  res.render("pages/index");
+});
+
+app.listen(port, () => {
+  console.log("app is listening");
+});
