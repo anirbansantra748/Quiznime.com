4ec5d388644f34e82766bafaf9e8b4778eaa78d3
AinzsOalGoan <deep951india@gmail.com>
2023-11-01 21:38:49 +0530

    Home page is updated and a new quiz overlay is added which is to be displayed for each question of quiz 1

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ app.get("/quizanime", (req, res) => {
 app.get("/quizanime/home", (req, res) => {
   res.render("pages/home.ejs");
 });
+app.get("/quizanime/Quiz1", (req, res) => {
+  res.render("pages/quiz_overlay.ejs");
+});
 
 
 
