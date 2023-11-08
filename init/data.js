c23209f70b21ae7fd50f142c72b0ff529b067f99
AinzsOalGoan <deep951india@gmail.com>
2023-11-08 20:51:05 +0530

    Data updated

--- init/data.js
+++ init/data.js
@@ -28,16 +28,17 @@ const questions = [
 		url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F4.bp.blogspot.com%2F-hZN-ovS0eII%2FWUS9iNKFq6I%2FAAAAAAAAFdQ%2F5rSS3FPeKpw862jWowZEoZGIOsfgFglfQCLcBGAs%2Fs1600%2Fnrtosh.jpg&f=1&nofb=1&ipt=81db4eb6d0514fc4fe60e46c1acb8a0c6ccf6c98abd53a268a87e071b7e01378&ipo=images",
 	},
 	{
-		question: 'In "Attack on Titan," what are the giant humanoid creatures known as?',
+		question:
+			'In "Attack on Titan," what are the giant humanoid creatures known as?',
 		options: ["Titans", "Zombies", "Mecha", "Kaiju"],
 		answer: "Titans",
-		url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages7.alphacoders.com%2F757%2F757109.jpg&f=1&nofb=1&ipt=e037390e9a9a3537364358f8a0ab1f448bdc469d31ea5f0f990ce506eef2265b&ipo=images",
+		url: "https://e1.pxfuel.com/desktop-wallpaper/228/191/desktop-wallpaper-watch-attack-on-titan-season-4-part-2-in-english-with-subs-and-online-for-in-india-or-worldwide-aot-2022.jpg",
 	},
 	{
 		question: 'What is the name of the VR set in "Sword Art Online"?',
 		options: ["VisionPro", "The NerveGear", "Aincrad", "ALfheim Online"],
 		answer: "The NerveGear",
-		url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wallpapertip.com%2Fwmimgs%2F99-994847_sword-art-online-aincrad-kirito.jpg&f=1&nofb=1&ipt=b1fbf12430f9914098b52adb77f2deb7baf3cb7ac317cbddff7f2c82d15ee24e&ipo=images",
+		url: "https://www.gamingscan.com/wp-content/uploads/2023/03/Sword-Art-Online-Games-In-Order.jpg",
 	},
 	{
 		question:
@@ -52,8 +53,7 @@ const questions = [
 		url: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F36600000%2FFull-Metal-Alchemist-image-full-metal-alchemist-36621492-4845-3263.jpg&f=1&nofb=1&ipt=da45a66eb38400f6e15063b5bdfcd5615868ad2a9cdf2dd312582bf9c08ecf3a&ipo=images",
 	},
 	{
-		question:
-			'What is the power of "One Punch Man" protagonist Saitama?',
+		question: 'What is the power of "One Punch Man" protagonist Saitama?',
 		options: [
 			"Super Speed",
 			"Teleportation",
