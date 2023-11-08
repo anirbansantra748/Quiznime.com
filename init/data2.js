68fad9e24a7fd6860002bea819c2e02f8b2258c3
AinzsOalGoan <deep951india@gmail.com>
2023-11-08 17:06:29 +0530

    Data changed and updated

--- init/data2.js
+++ init/data2.js
@@ -1,33 +1,83 @@
 const images = [
-  {
-    url:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Ff4%2F51%2F0d%2Ff4510d2f883ea8c154080c6330f0602b--monkey-d-luffy-monkeys.jpg&f=1&nofb=1&ipt=dd8cc80c336ace94df7f1999c261fb9015143df6f3afb1380d011304d668435f&ipo=images",
-    options: ['Luffy D. Monkey', 'Naruto Uzumaki', 'Ichigo Kurosaki', 'Gon Freecss'],
-    answer:"Luffy D. Monkey",
-  },
-  {
-    url:"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F35700000%2FLight-Yagami-death-note-35716957-1440-900.jpg&f=1&nofb=1&ipt=f358a4dec97f07eabcc31e254d4fdc551c07f1b3b6bc313ec1b6fb0f0d7cb8ba&ipo=images",
-    options: ['Ryuk', 'Light Yagami', 'L Lawliet', 'Misa Amane'],
-    answer:"Light Yagami",
-  },
-  {
-    url:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F4.bp.blogspot.com%2F-hZN-ovS0eII%2FWUS9iNKFq6I%2FAAAAAAAAFdQ%2F5rSS3FPeKpw862jWowZEoZGIOsfgFglfQCLcBGAs%2Fs1600%2Fnrtosh.jpg&f=1&nofb=1&ipt=81db4eb6d0514fc4fe60e46c1acb8a0c6ccf6c98abd53a268a87e071b7e01378&ipo=images",
-    options: ['Naruto', 'Bleach', 'Dragon Ball Z', 'My Hero Academia'],
-    answer:"Naruto",
-  },
-  {
-    url:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nawpic.com%2Fmedia%2F2020%2Fone-punch-man-nawpic-11.jpg&f=1&nofb=1&ipt=b5c97cca1e7b1ba891c29fde5dd825e05d3f7f735ec5e4ef838f95226962d8b3&ipo=images",
-    options: ['Ryuk', 'Light Yagami', 'Saitama', 'Misa Amane'],
-    answer:"Saitama",
-  },
-  {
-    url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fbleach%2Fimages%2F2%2F20%2FIchigo_Kurosaki.png&f=1&nofb=1&ipt=76abceca6c55a5c51b58ff264a0a89c76cbfe888dd0e0af9be7e2c14d3d129ed&ipo=images",
-    options: ['Byakuya Kuchiki', 'Renji Abarai', 'Uryu Ishida', 'Ichigo Kurosaki'],
-    answer: "Ichigo Kurosaki",
-  },
-  {
-    url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fhunterxhunter%2Fimages%2F0%2F0d%2FGon_Freecss.png&f=1&nofb=1&ipt=3064f2e87f7381d0b9318dd9259da708b777cc4775c1a35c7d4f531fb6e4b218&ipo=images",
-    options: ['Killua Zoldyck', 'Gon Freecss', 'Hisoka', 'Leorio'],
-    answer: "Gon Freecss",
-  },
+	{
+		url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2Ff4%2F51%2F0d%2Ff4510d2f883ea8c154080c6330f0602b--monkey-d-luffy-monkeys.jpg&f=1&nofb=1&ipt=dd8cc80c336ace94df7f1999c261fb9015143df6f3afb1380d011304d668435f&ipo=images",
+		options: [
+			"Luffy D. Monkey",
+			"Naruto Uzumaki",
+			"Misa Amane",
+			"Gon Freecss",
+		],
+		answer: "Luffy D. Monkey",
+	},
+	{
+		url: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages6.fanpop.com%2Fimage%2Fphotos%2F35700000%2FLight-Yagami-death-note-35716957-1440-900.jpg&f=1&nofb=1&ipt=f358a4dec97f07eabcc31e254d4fdc551c07f1b3b6bc313ec1b6fb0f0d7cb8ba&ipo=images",
+		options: ["Denji", "Light Yagami", "Levi Akerman", "Arataka Reigen"],
+		answer: "Light Yagami",
+	},
+	{
+		url: "https://staticg.sportskeeda.com/editor/2022/07/015d8-16587443922786-1920.jpg?w=840",
+		options: [
+			"Sasuke Uchiha",
+			"Light Yagami",
+			"Saitama",
+			"Shirogane Myuki",
+		],
+		answer: "Saitama",
+	},
+	{
+		url: "https://staticg.sportskeeda.com/editor/2023/10/ca674-16978248911926-1920.jpg",
+		options: ["Frieren", "Gon Freecss", "Himawari Uzumaki", "Gojo Satoru"],
+		answer: "Frieren",
+	},
+	{
+		url: "https://steamuserimages-a.akamaihd.net/ugc/887629915513898608/E879C523124E620114178D9B74D324096B13DECB/",
+		options: [
+			"Killua Zoldyck",
+			"Jabami Yumeko",
+			"Himeno",
+			"Ichigo Kurosaki",
+		],
+		answer: "Jabami Yumeko",
+	},
+	{
+		url: "https://gonkilluasite.files.wordpress.com/2016/04/tumblr_o49m0by74z1ruc976o3_1280.jpg?w=640",
+		options: ["Sukuna", "Gon Freecss", "Rimuru Tempest", "Eren Yeager"],
+		answer: "Gon Freecss",
+	},
+	{
+		url: "https://pbs.twimg.com/media/E1KNj5aVEAop8Lb.jpg:large",
+		options: [
+			"Killua Zoldyck",
+			"Kageyama Shigeo",
+			"Itachi Uchiha",
+			"Albedo",
+		],
+		answer: "Kageyama Shigeo",
+	},
+	{
+		url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F4.bp.blogspot.com%2F-hZN-ovS0eII%2FWUS9iNKFq6I%2FAAAAAAAAFdQ%2F5rSS3FPeKpw862jWowZEoZGIOsfgFglfQCLcBGAs%2Fs1600%2Fnrtosh.jpg&f=1&nofb=1&ipt=81db4eb6d0514fc4fe60e46c1acb8a0c6ccf6c98abd53a268a87e071b7e01378&ipo=images",
+		options: ["Naruto", "Ichigo Kurosaki", "Son Goku", "Ryuk"],
+		answer: "Naruto",
+	},
+	{
+		url: "https://c4.wallpaperflare.com/wallpaper/926/458/556/anime-anime-girls-anime-screenshot-eighty-six-vladilena-milize-hd-wallpaper-preview.jpg",
+		options: [
+			"Bocchi",
+			"Vladilena Milizé",
+			"Violet Evergarden",
+			"Ilumi Zoldyck",
+		],
+		answer: "Vladilena Milizé",
+	},
+	{
+		url: "https://practicaltyping.com/wp-content/uploads/2022/06/kaguya.jpg",
+		options: [
+			"Killua Zoldyck",
+			"Chihiro Ogino",
+			"Kaguya Shinomiya",
+			"Itadori Yuji",
+		],
+		answer: "Kaguya Shinomiya",
+	},
 ];
 module.exports = { data2: images };
